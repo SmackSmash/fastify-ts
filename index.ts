b5478d16f37e4ca282afaee25f58057eefda0f67
Dan Moffat <danmoffat@gmail.com>
2024-05-03 22:58:49 +0100

    Fixed imports and type errors

--- index.ts
+++ index.ts
@@ -1,16 +1,38 @@
-import Fastify from 'fastify';
-import testRoute from './routes/test';
+const Fastify = require('fastify');
+const fastifyEnv = require('@fastify/env');
+const testRoute = require('./routes/test');
 
 const fastify = Fastify({
   logger: true
 });
+
+const schema = {
+  type: 'object',
+  required: ['MONGO_URI'],
+  properties: {
+    MONGO_URI: {
+      type: 'string'
+    }
+  }
+};
+
+fastify
+  .register(fastifyEnv, {
+    confKey: 'config',
+    schema: schema,
+    dotenv: true
+  })
+  .ready((error: unknown) => {
+    console.error(error);
+    console.log(fastify.config.MONGO_URI);
+  });
 
 fastify.register(testRoute);
 
 (async () => {
   try {
     await fastify.listen({ port: 3000 });
-  } catch (error) {
+  } catch (error: unknown) {
     fastify.log.error(error);
     process.exit(1);
   }
